dda821c598a1c8787bc5f16c76302dedd3b5913a
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2022-02-27 20:32:57 +0100

    added properties to Item

--- FolkDanceTime.Bll/Entities/Item.cs
+++ FolkDanceTime.Bll/Entities/Item.cs
@@ -4,13 +4,15 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? PictureFilename { get; set; }
         public int UserId { get; set; }
         public int CategoryId { get; set; }
         public int? ItemSetId { get; set; }
 
         public virtual User User { get; set; }
         public virtual Category Category { get; set; }
-        public virtual ItemSet ItemSet { get; set; }
+        public virtual ItemSet? ItemSet { get; set; }
         public virtual ItemTransaction ItemTransaction { get; set; }
         public virtual List<PropertyValue> PropertyValues { get; set; }
     }
