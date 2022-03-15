290e259870f8fcef3cfd17f85d19fdf782a86111
Patrik Ángyán <58144458+MrPatesz@users.noreply.github.com>
2022-03-15 22:36:50 +0100

    Category Service and Controller with CRUD actions

---
+++ FolkDanceTime.Bll/Services/CategoryService.cs
@@ -0,0 +1,69 @@
+﻿using FolkDanceTime.Dal.Data;
+using FolkDanceTime.Dal.Entities;
+
+namespace FolkDanceTime.Bll.Services
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<Category> GetCategories()
+        {
+            return dbContext.Categories.ToList();
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            var category = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+
+            return category;
+        }
+
+        public async Task<Category> AddCategory(Category category)
+        {
+            dbContext.Categories.Add(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<bool> DeleteCategoryById(int id)
+        {
+            var toRemove = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (toRemove != null)
+            {
+                dbContext.Categories.Remove(toRemove);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<Category> EditCategory(Category category)
+        {
+            var toEdit = dbContext.Categories.FirstOrDefault(c => c.Id == category.Id);
+
+            if (toEdit == null)
+            {
+                return null;
+            }
+
+            toEdit.Name = category.Name;
+
+            dbContext.Categories.Update(toEdit);
+            await dbContext.SaveChangesAsync();
+
+            return toEdit;
+        }
+    }
+}
---
+++ FolkDanceTime.Web/Server/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using FolkDanceTime.Bll.Services;
+using FolkDanceTime.Dal.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FolkDanceTime.Web.Server.Controllers
+{
+    [Route("api/categories")]
+    [Authorize]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryService categoryService;
+
+        public CategoryController(CategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Category>> GetCategories()
+        {
+            return Ok(categoryService.GetCategories());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Category> GetCategoryById(int id)
+        {
+            var album = categoryService.GetCategoryById(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(album);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Category>> AddCategory([FromBody] Category category)
+        {
+            var newCategory = await categoryService.AddCategory(category);
+            if (newCategory == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(newCategory);
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Category>> EditAlbum([FromBody] Category category)
+        {
+            var editedCategory = await categoryService.EditCategory(category);
+            if (editedCategory == null)
+                return BadRequest();
+            else
+                return Ok(editedCategory);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> DeleteCategoryById(int id)
+        {
+            if (await categoryService.DeleteCategoryById(id))
+                return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
